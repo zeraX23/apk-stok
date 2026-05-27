a202e717e32261d02f61f916bc85b48e66aba43b
zeraX23 <officialjiyora@gmail.com>
2026-05-27 14:16:09 +0700

    Create Color.kt

---
+++ app/src/main/java/com/zerax23/stokbarcode/ui/theme/Color.kt
@@ -0,0 +1,34 @@
+package com.zerax23.stokbarcode.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary
+val Green800 = Color(0xFF1B5E20)
+val Green600 = Color(0xFF388E3C)
+val Green200 = Color(0xFFA5D6A7)
+
+// Secondary
+val Blue800 = Color(0xFF1565C0)
+val Blue600 = Color(0xFF1976D2)
+val Blue200 = Color(0xFF90CAF9)
+
+// Background
+val BackgroundLight = Color(0xFFF8F9FA)
+val BackgroundDark = Color(0xFF121212)
+val SurfaceLight = Color(0xFFFFFFFF)
+val SurfaceDark = Color(0xFF1E1E1E)
+
+// Category Colors
+val CategoryElektronik = Color(0xFF1565C0)
+val CategoryMakanan = Color(0xFFE65100)
+val CategoryMinuman = Color(0xFF00838F)
+val CategoryPakaian = Color(0xFF6A1B9A)
+val CategoryPerabot = Color(0xFF4E342E)
+val CategoryKesehatan = Color(0xFF2E7D32)
+val CategoryKosmetik = Color(0xFFAD1457)
+val CategoryLainnya = Color(0xFF37474F)
+
+// Status Colors
+val StatusAvailable = Color(0xFF2E7D32)
+val StatusSold = Color(0xFFC62828)
+val StatusReturned = Color(0xFFE65100)
